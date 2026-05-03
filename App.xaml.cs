7ec4f49dd746958c0bc442e51cec702485e279bf
AzukiSan-png <trachilyubo2@gmail.com>
2026-05-03 15:09:34 +0900

    DPIスケーリングの問題を修正

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Runtime.InteropServices;
 using Microsoft.UI.Xaml;
 
 namespace XTimelineViewer
@@ -6,8 +8,13 @@ namespace XTimelineViewer
     {
         private Window? _window;
 
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDpiAwarenessContext(IntPtr dpiContext);
+        private static readonly IntPtr DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 = new IntPtr(-4);
+
         public App()
         {
+            SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
             this.InitializeComponent();
         }
 
